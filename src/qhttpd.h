af20b32547fac6db37e24edc2bc1e6efc45898f3
wolkykim <wolkykim@gmail.com>
2008-07-01 11:24:51 +0000

    git-svn-id: https://svn.qdecoder.org/qhttpd/trunk@28 915dc90f-ddda-dc11-960a-0015c5f490bc

--- src/qhttpd.h
+++ src/qhttpd.h
@@ -69,7 +69,7 @@
 #define MAX_LOGLEVEL				(4)
 
 #define	MAX_PATH_LEN				(1024)
-#define	MAX_FILEPATH_LENGTH			(1024*4-1)
+#define	MAX_FILEPATH_LENGTH			((1024*4)-1)
 #define	MAX_FILENAME_LENGTH			(256-1)
 #define LINEBUF_SIZE				(1024)
 
