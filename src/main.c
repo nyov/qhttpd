43d2daed9d7329010649f8864d808c101936fc4f
wolkykim <wolkykim@gmail.com>
2010-01-27 23:59:24 +0000

    git-svn-id: https://svn.qdecoder.org/qhttpd/trunk@86 915dc90f-ddda-dc11-960a-0015c5f490bc

--- src/main.c
+++ src/main.c
@@ -111,6 +111,7 @@ int main(int argc, char *argv[]) {
 	if(bConfigLoadStatus == false) {
 		fprintf(stderr, "ERROR: Can't load configuration.\n");
 		fprintf(stderr, "       Use '-d' option to see more details.\n");
+		fprintf(stderr, "       Use '-h' option for help messages.\n");
 		//printUsages();
 		return EXIT_FAILURE;
 	}
